9fab8b65e78c1e057f8a968f5cdff0dba7081903
Kemény Boldi <kemeny.boldi@gmail.com>
2017-08-27 17:55:02 +0200

    Edit regular font

--- canvases/elements/constants.js
+++ canvases/elements/constants.js
@@ -26,7 +26,7 @@ module.exports = {
             bold: 'bold 60px SFUIDisplay',
             semibold: '600 60px SFUIDisplay',
             light: '300 60px SFUIDisplay',
-            regular: 'regular 60px SFUIDisplay',
+            regular: '60px SFUIDisplay',
             medium: '500 60px SFUIDisplay'
         },
         px50: {
