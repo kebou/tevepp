9d9916a8a9982f6b991119d4a0bc5bf9e4a1a7fc
Kemény Boldi <kemeny.boldi@gmail.com>
2017-09-22 08:42:45 +0200

    Add loadtest webhook

--- modules/bootbot/webhooks.js
+++ modules/bootbot/webhooks.js
@@ -6,6 +6,9 @@ module.exports = (bot) => {
     if (!(bot instanceof BootBot)) {
         throw new Error('This modul has to be required with an instance of BootBot.');
     }
+    const userController = require('../../controllers/userController')(bot);
+    const locationController = require('../../controllers/locationController');
+    const TripPlanning = require('../../intents/tripPlanning')(bot);
 
     bot.app.get('/image/:imageId', (req, res) => {
         const imgId = req.params.imageId;
@@ -21,5 +24,21 @@ module.exports = (bot) => {
                 res.send('Image Not Found');
                 res.end();
             });
+    });
+
+    bot.app.get('/loadtest', (req, res) => {
+        let user, start;
+        userController.getUser('1405829276203024')
+            .then(res => {
+                user = res;
+                return locationController.fromText('albertfalva utca', user.id);
+            })
+            .then(res => {
+                start = res;
+                return locationController.fromText('szentendre', user.id);
+            })
+            .then(stop => TripPlanning.planTrip(user, start, stop))
+            .then(() => res.sendStatus(200))
+            .catch(console.error);
     });
 };
