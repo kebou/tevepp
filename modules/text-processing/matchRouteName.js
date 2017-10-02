46d434034cf34736cd6ff0c4495720a4739c3629
Kemény Boldi <kemeny.boldi@gmail.com>
2017-10-02 21:42:53 +0200

    Alter route name matching

--- modules/text-processing/matchRouteName.js
+++ modules/text-processing/matchRouteName.js
@@ -1,14 +1,34 @@
 'use strict';
 const Pattern = require('../../utils/patterns');
 /**
- * In: text
+ * In: tokens, start, end
  * Out: routeName
  */
 module.exports = (ctx, next) => {
+    const { start, end, tokens } = ctx;
+    if (!tokens) {
+        console.error('matchRouteName module should be used after "tokens" property in ctx');
+        return next();
+    }
+    
+    let tokensToProcess = tokens.filter(token => filterTokens(token, start, end));
     const pattern = Pattern.routeNameInText();
-    const match = ctx.text.match(pattern);
-    if (match) {
-        ctx.routeName = match[0];
+
+    for (let token of tokensToProcess) {
+        const match = token.content && token.content.match(pattern);
+        if (match) {
+            ctx.routeName = match[0];
+        }
     }
     return next();
+};
+
+const filterTokens = (token, start, end) => {
+    if (start && start.tokens && start.tokens.findIndex(x => x.id === token.id) >= 0) {
+        return false;
+    }
+    if (end && end.tokens && end.tokens.findIndex(x => x.id === token.id) >= 0) {
+        return false;
+    }
+    return true;
 };
