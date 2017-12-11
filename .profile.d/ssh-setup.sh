070e4614481a232a122573e91b9d3d541ea4a0b3
Kemény Boldi <kemeny.boldi@gmail.com>
2017-12-11 23:16:22 +0100

    Add ssh-setup

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -6,11 +6,11 @@ mkdir -p $HOME/.ssh
 chmod 700 $HOME/.ssh
 
 # Create the public and private key files from the environment variables.
-echo "${4LANG_PUBLIC_KEY}" > $HOME/.ssh/heroku_id_rsa.pub
+echo ${4LANG_PUBLIC_KEY} > $HOME/.ssh/heroku_id_rsa.pub
 chmod 644 $HOME/.ssh/heroku_id_rsa.pub
 
 # Note use of double quotes, required to preserve newlines
-echo "${4LANG_PRIVATE_KEY}" > $HOME/.ssh/heroku_id_rsa
+echo ${4LANG_PRIVATE_KEY} > $HOME/.ssh/heroku_id_rsa
 chmod 600 $HOME/.ssh/heroku_id_rsa
 
 # Preload the known_hosts file  (see "version 2" below)
