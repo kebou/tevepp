c264ff027b1328ba3916478141872c37766de5d9
Kemény Boldi <kemeny.boldi@gmail.com>
2017-12-11 21:04:00 +0100

    Add aliasController

---
+++ controllers/aliasController.js
@@ -0,0 +1,54 @@
+'use strict';
+const Alias = require('../models/aliasModel');
+const latinize = require('latenize');
+
+let table, pattern;
+
+const replace = (str) => {
+    return str.replace(pattern, replacer);
+};
+
+const replacer = (match, p1, p2, p3, offset, string) => {
+    const name = latinize(match).toLowerCase();
+    return table[name].value || string;
+};
+
+const update = () => {
+    return Alias.find({})
+        .then(aliases => {
+            table = generateAlternatives(aliases);
+            pattern = generatePattern(table);
+            console.log(Object.keys(table));
+        });
+};
+
+const generateAlternatives = (aliases) => {
+    const table = {};
+    for (let alias of aliases) {
+        const alternatives = sortAlternatives(alias.alternatives);
+        const value = alias.value;
+        for (let alt of alternatives) {
+            const name = latinize(alt.name).toLowerCase();
+            table[name] = {
+                pattern: alt.pattern || name,
+                value
+            };
+        }
+    }
+    return table;
+};
+
+const sortAlternatives = (alternatives) => {
+    return alternatives.sort((a, b) => b.name.length - a.name.length);
+};
+
+const generatePattern = (alternatives) => {
+    return new RegExp(Object.keys(alternatives).map(x => alternatives[x].pattern).join('|'), 'gi');
+};
+
+update();
+
+module.exports = {
+    update,
+    replace
+};
