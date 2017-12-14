c11fa60b3250757be7a5d21cdfc948c8ae8cedb5
Kemény Boldi <kemeny.boldi@gmail.com>
2017-12-14 01:42:47 +0100

    Add typing

--- messages/schedule/invalidStopNameMessage.js
+++ messages/schedule/invalidStopNameMessage.js
@@ -7,8 +7,9 @@ module.exports = (bot) => {
     const invalidStopNameMessage = (user) => {
         const text = Text.schedule.invalidStopName(user);
         const quickReplies = QR.menu(user);
+        const options = { typing: 1 };
 
-        return bot.sendTextMessage(user.id, text, quickReplies);
+        return bot.sendTextMessage(user.id, text, quickReplies, options);
     };
 
     return invalidStopNameMessage;
