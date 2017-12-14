0f36a45c2c5557319605fc2ef92dc4679a809a7f
Kemény Boldi <kemeny.boldi@gmail.com>
2017-12-14 01:36:01 +0100

    Testing

--- index.js
+++ index.js
@@ -158,12 +158,12 @@ const mapOpts = {
     region: 'hu',
     apiKey: process.env.MAPS_API_KEY,
     formatter: null,
-    excludePartialMatches: true,
+    excludePartialMatches: false,
     bounds: '47.1523107,18.8460594|47.6837053,19.3915303'
 };
 
 const gc = NodeGeocoder(mapOpts);
-gc.geocode({ address: 'setany', country: 'Magyarország', minConfidence: 0, withBounds: true })
+gc.geocode({ address: 'qqqqqqqqqqqqqqqqq', country: 'Magyarország', minConfidence: 0, withBounds: true })
     .then(res => {
         console.log(res);
         process.exit(0);
