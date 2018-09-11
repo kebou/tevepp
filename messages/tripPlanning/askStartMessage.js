09ac87bf5beaf7d4ff7d54fb62a76f0365de6d17
kebou <kemeny.boldi@gmail.com>
2018-09-11 21:24:14 +0200

    Change quickreply order

--- messages/tripPlanning/askStartMessage.js
+++ messages/tripPlanning/askStartMessage.js
@@ -6,7 +6,11 @@ module.exports = (bot) => {
     const askStartMessage = (user) => {
         const text = Text.tripPlanning.askStart(user);
         const hint = Text.locationHint(user);
-        const quickReplies = [QR.location(), ...QR.favouriteLocations(user), QR.cancel(user)];
+        const quickReplies = [
+            QR.cancel(user),
+            QR.location(),
+            ...QR.favouriteLocations(user)
+        ];
         const options = { typing: true };
         return bot.sendTextMessage(user.id, text, [], options)
             .then(() => bot.sendTextMessage(user.id, hint, quickReplies));
